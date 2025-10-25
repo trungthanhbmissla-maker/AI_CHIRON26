f02e01a814b53368aa21e9c8b2fff517b890d88c
Trung Thanh <trungthanhbmissla@gmail.com>
2025-10-26 02:54:15 +0700

    Fix

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bash start.sh
+web: gunicorn backend.app:app
--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Khởi động Flask (chạy nền) + Streamlit (chạy chính)
-python BACKEND_FLASK/app.py &
-streamlit run FRONTEND_STREAMLIT/chiron26.py --server.port=$PORT --server.address=0.0.0.0
